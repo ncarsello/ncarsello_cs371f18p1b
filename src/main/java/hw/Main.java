b78c6adcab1a6940fb33e440a83c353341d8b4b4
nicholas <ncarsello@luc.edu>
2018-09-13 20:04:07 -0500

    The project is done.

--- src/main/java/hw/Main.java
+++ src/main/java/hw/Main.java
@@ -1,14 +1,53 @@
 package hw;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
 
   public static void main(final String[] args) {
-    System.out.println("args = " + Arrays.asList(args));
-    final HelloWorld instance = new HelloWorld();
-    System.out.println(instance.getMessage());
-    System.out.println(instance.getYear());
-    System.out.println("bye for now");
+
+    System.out.println("./topword");
+    Scanner keyboard = new Scanner(System.in);
+
+
+    final int howmany = keyboard.nextInt();
+    final int minlength = keyboard.nextInt();
+    final int lastnwords = keyboard.nextInt();
+
+    Scanner input = new Scanner(System.in);
+
+    final Queue<String> cloudinput = new LinkedList<>();
+    final Map<String, Integer> cloudoutput = new HashMap<>();
+
+
+    while (input.hasNext() && (cloudinput.size()<lastnwords)) {
+      String line = input.next();
+      if (line.length()>=minlength) {
+        cloudinput.add(line);
+      }
+
+      }
+    while (!cloudinput.isEmpty()) {
+      String word = cloudinput.remove();
+      if (cloudoutput.containsKey(word)) {
+        cloudoutput.put(word,cloudoutput.get(word)+1);
+      }
+      else{
+        cloudoutput.put(word, 1);
+      }
+    }
+    ArrayList<String> output = new ArrayList<>();
+    cloudoutput.entrySet().
+            stream().
+            sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).
+            forEachOrdered(x -> output.add(x.getKey()));
+
+    int count = 0;
+    while(count < howmany && count < output.size()) {
+      System.out.println(output.get(count) + ": " + cloudoutput.get(output.get(count)));
+      count++;
+    }
   }
+
+
 }
